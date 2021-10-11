c7a35b98f3d0446db40cf4740c45da7ecc3c38b4
thomas <thomas.drege@testapic.com>
2021-10-11 11:06:16 +0200

    Full screen

--- app/src/main/java/com/example/utb/MainActivity.kt
+++ app/src/main/java/com/example/utb/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.example.utb
 
 import android.app.AlertDialog
-import android.media.AudioManager
 import android.os.Build
 import android.os.Bundle
 import android.os.Handler
@@ -43,6 +42,13 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     private var countEnding: Int = 1
     private var video: Int = R.raw.intro
 
+    private val UI_OPTIONS = (View.SYSTEM_UI_FLAG_LOW_PROFILE
+            or View.SYSTEM_UI_FLAG_FULLSCREEN
+            or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+            or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+            or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+            or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         initWindowView()
@@ -78,16 +84,13 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     private fun initWindowView() {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main)
-
         supportActionBar?.hide()
+
         val layout = window.attributes
         layout.screenBrightness = 1f
-
         window.attributes = layout
-        this.window.setFlags(
-            WindowManager.LayoutParams.FLAG_FULLSCREEN,
-            WindowManager.LayoutParams.FLAG_FULLSCREEN
-        )
+
+        window.decorView.systemUiVisibility = UI_OPTIONS
 
     }
 
